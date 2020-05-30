0ffe77a2336dbb5222d9d1265cea8c025df0a9bd
insac81196 <insac81196@gmail.com>
2020-05-31 00:11:37 +0700

    make it better

--- routerHttp.js
+++ routerHttp.js
@@ -5,13 +5,13 @@ let mobile = require('is-mobile');
 module.exports = function(app, redT) {
 	//Home
 	app.get('/', function(req, res) {
-		if (mobile({ua:req})){
-			return res.redirect('/mobile');
-		} else {
+		//if (mobile({ua:req})){
+			//return res.redirect('/mobile');
+		//} else {
 			return res.redirect('/web');
-		}
+		//}
 	});
 
-	// Sign API
+	//Sign API
 	require('./routes/api')(app, redT);  // load routes API
 };
